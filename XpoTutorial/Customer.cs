bd4dc478c196da309e4e6c56fa595c512fa17159
Joche <Joche@DESKTOP-AT8AVUQ>
2018-06-22 11:45:05 +0300

    Add project files.

---
+++ XpoTutorial/Customer.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XpoTutorial
+{
+    public class Customer: XPObject
+    {
+        public Customer()
+        {
+
+        }
+
+        public Customer(Session session) : base(session)
+        {
+
+        }
+
+        public Customer(Session session, XPClassInfo classInfo) : base(session, classInfo)
+        {
+
+        }
+
+        string name;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+
+
+
+    }
+}
---
+++ XpoTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+
+namespace XpoTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Hack lets setup the data layer
+            //Hack vamos a configurar el data layer
+            var ConnectionString = AccessConnectionProvider.GetConnectionString("MyDatabase.mdb");
+
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(ConnectionString, AutoCreateOption.DatabaseAndSchema);
+
+            //HACK our first XPO Object Instance
+            //HACK nuestra primera instancia de un objeto XPO
+
+            Customer Customer = new Customer(XpoDefault.Session);
+            Customer.Name = "Jose Manuel Ojeda Melgar";
+            Customer.Save();
+            
+
+
+
+        }
+    }
+}
